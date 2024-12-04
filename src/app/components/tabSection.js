2d746b3fad04aa48e624b3a2c9551229804c7af4
ZikriaChuhan <chuhanzikria@gmail.com>
2024-12-04 18:01:33 +0500

    first commit

--- src/app/components/tabSection.js
+++ src/app/components/tabSection.js
@@ -1,4 +1,5 @@
 "use client";
+
 import { useState, useEffect } from "react";
 import Swal from "sweetalert2";
 import Image from "next/image";
@@ -15,6 +16,7 @@ export default function TabSection() {
 
 
 
+
   const tabs = ["All Meals", "Week 1", "Week 2", "Week 3", "Week 4"];
 
   useEffect(() => {
@@ -25,7 +27,7 @@ export default function TabSection() {
         setRecipes(data.recipes);
       } catch (error) {
         console.error("Error fetching data:", error);
-      } 
+      }
     };
     fetchData();
   }, []);
@@ -115,22 +117,7 @@ export default function TabSection() {
                     </div>
                   </div>  ))
 ) : (
-  <Card className="w-[200px] space-y-5 p-4" radius="lg">
-  <Skeleton className="rounded-lg">
-    <div className="h-24 rounded-lg bg-default-300"></div>
-  </Skeleton>
-  <div className="space-y-3">
-    <Skeleton className="w-3/5 rounded-lg">
-      <div className="h-3 w-3/5 rounded-lg bg-default-200"></div>
-    </Skeleton>
-    <Skeleton className="w-4/5 rounded-lg">
-      <div className="h-3 w-4/5 rounded-lg bg-default-200"></div>
-    </Skeleton>
-    <Skeleton className="w-2/5 rounded-lg">  
-      <div className="h-3 w-2/5 rounded-lg bg-default-300"></div>
-    </Skeleton>
-  </div>
-</Card>
+  <p>No meal available</p>
 )}
               </div>
             )}
